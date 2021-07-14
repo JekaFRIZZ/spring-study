859cfebb290b836883eb16eb53f86d3c06029bd0
Женя <lapko.eugene@gmail.com>
2021-07-14 18:02:37 +0300

    lesson 7: @Qualifier

--- src/main/java/com/studyspring/MusicPlayer.java
+++ src/main/java/com/studyspring/MusicPlayer.java
@@ -1,11 +1,13 @@
 package com.studyspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MusicPlayer {
     @Autowired
+    @Qualifier("rokMusic")
     private Music music;
 
     public void playingMusic() {
