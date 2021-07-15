415b93b746d7a42b18b6ebefe74a3afa329c2293
Женя <lapko.eugene@gmail.com>
2021-07-15 11:18:59 +0300

    lesson 8: Configuration without xml

---
+++ src/main/java/com/studyspring/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.studyspring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.studyspring")
+public class SpringConfig {
+
+}
