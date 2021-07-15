4a96378d92461b81a0c9876fddfdae9e3ee037b3
Женя <lapko.eugene@gmail.com>
2021-07-15 16:44:39 +0300

    lesson 13: Model

--- src/main/java/com/studyspring/controllers/FirstController.java
+++ src/main/java/com/studyspring/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.studyspring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,8 +10,9 @@ public class FirstController {
 
     @GetMapping("/hello")
     public String helloPage(@RequestParam("name") String name ,
-                            @RequestParam("surname") String surname) {
-        System.out.println("Hello " + name + " " + surname);
+                            @RequestParam("surname") String surname,
+                            Model model) {
+        model.addAttribute("message", "Hello " + name + " " + surname);
         return "first/hello";
     }
 
